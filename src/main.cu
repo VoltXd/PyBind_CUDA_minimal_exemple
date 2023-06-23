642ac00718ad90f3ea951c59e133469f85cf5490
VoltXd <pierrealexandre.peyronnet@gmail.com>
2023-06-23 17:02:36 +0200

    Gauss Random values test

--- src/main.cu
+++ src/main.cu
@@ -1,8 +1,20 @@
 #include <pybind11/pybind11.h> 
 #include <pybind11/numpy.h> 
 #include <vector>
+#include <random>
+
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "curand.h"
+#include "curand_kernel.h"
 
 #include <iostream>
+
+constexpr size_t CUDA_THREADS_PER_BLOCK = 1024;
+constexpr size_t CUDA_BLOCKS = 32;
+
+constexpr size_t CUDA_THREADS_COUNT = CUDA_BLOCKS * CUDA_THREADS_PER_BLOCK;
 
 namespace py = pybind11;
 
@@ -36,7 +48,7 @@ py::array_t<double> CUDA_addVector(py::array_t<double>& a, py::array_t<double>& 
     cudaMemcpy(cuPtr_a, a.data(), a.size() * sizeof(double), cudaMemcpyHostToDevice);
     cudaMemcpy(cuPtr_b, b.data(), a.size() * sizeof(double), cudaMemcpyHostToDevice);
 
-    addVector<<<32,1024>>>(cuPtr_a, cuPtr_b, cuPtr_c, a.size());
+    addVector<<<CUDA_BLOCKS,CUDA_THREADS_PER_BLOCK>>>(cuPtr_a, cuPtr_b, cuPtr_c, a.size());
 
     std::vector<double> c(a.size());
     cudaMemcpy(c.data(), cuPtr_c, a.size() * sizeof(double), cudaMemcpyDeviceToHost);
@@ -46,9 +58,64 @@ py::array_t<double> CUDA_addVector(py::array_t<double>& a, py::array_t<double>& 
     return py::array(c.size(), c.data());
 }
 
+py::array_t<double> getRandomGaussVector(double mean, double stddev, uint64_t N)
+{
+    std::random_device r;
+    std::default_random_engine generator { r() };
+    std::normal_distribution gaussianDistribution(mean, stddev);
+
+    std::vector<double> randomNumbers(N);
+    for (double& d : randomNumbers)
+        d = gaussianDistribution(generator);
+
+    return py::array(randomNumbers.size(), randomNumbers.data());
+}
+
+__global__ void kernel_curandSetup(curandState* globalState, uint64_t seed)
+{
+    size_t threadIndex = threadIdx.x + blockDim.x * blockIdx.x;
+    curand_init(seed, threadIndex, 0, &globalState[threadIndex]);
+}
+
+__global__ void kernel_getRandomGaussVector(curandState* globalState, double* v, double mean, double stddev, uint64_t N)
+{
+    const size_t startIndex = threadIdx.x + blockDim.x * blockIdx.x;
+    const size_t step = blockDim.x * gridDim.x;
+    
+    curandState localState = globalState[startIndex];
+
+    
+    for (uint64_t idx = startIndex; idx < N; idx += step)
+        v[idx] = stddev * curand_normal(&localState) + mean;
+
+    
+    globalState[startIndex] = localState;
+}
+
+py::array_t<double> getRandomGaussVector_cuda(double mean, double stddev, uint64_t N)
+{
+    // Random device allocation
+    curandState* cuPtr_deviceCurandStates;
+    cudaMalloc(&cuPtr_deviceCurandStates, CUDA_THREADS_COUNT * sizeof(curandState));
+    kernel_curandSetup<<<CUDA_BLOCKS,CUDA_THREADS_PER_BLOCK>>>(cuPtr_deviceCurandStates, time(NULL));
+
+    double* cuPtr_randomNumbers;
+    cudaMalloc(&cuPtr_randomNumbers, N * sizeof(double));
+
+    kernel_getRandomGaussVector<<<CUDA_BLOCKS,CUDA_THREADS_PER_BLOCK>>>(cuPtr_deviceCurandStates, cuPtr_randomNumbers, mean, stddev, N);
+
+    std::vector<double> randomNumbers(N);
+    cudaMemcpy(randomNumbers.data(), cuPtr_randomNumbers, N * sizeof(double), cudaMemcpyDeviceToHost);
+
+    return py::array(randomNumbers.size(), randomNumbers.data());
+}
+
+
 PYBIND11_MODULE(TEST_PYBIND_CUDA, handle)
 {
     handle.doc() = "Docu";
     handle.def("add", &add);
     handle.def("add_cuda", &CUDA_addVector);
+    handle.def("get_random_gauss_vector", &getRandomGaussVector);
+    handle.def("get_random_gauss_vector_cuda", &getRandomGaussVector_cuda);
 }
--- test.py
+++ test.py
@@ -1,8 +1,37 @@
 import build.Release.TEST_PYBIND_CUDA as E
 import numpy as np
+import matplotlib.pyplot as plt
 
 size = int(1e6)
-a = np.ones(size)
-b = 2 * np.ones(size)
 
-print(E.add_cuda(a, b))
+param = [[0, 1], [3, 2], [-5, 0.5]]
+legend_str = []
+
+colors = ["blue", "red", "green"]
+
+
+for i in range(len(param)):
+    mu = param[i][0]
+    sigma = param[i][1]
+
+    a = E.get_random_gauss_vector(mu, sigma, size)
+
+    num_bins = 50
+
+    n, bins, patches = plt.hist(a, num_bins, density=1, color=colors[i], alpha=0.7)
+
+    y = ((1.0 / (np.sqrt(2 * np.pi) * sigma)) * np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
+
+    plt.plot(bins, y, '--', color ="black")
+    
+    plt.xlabel('X')
+    plt.ylabel('Density')
+
+    legend_str.append("Normal probability density")
+
+for i in range(len(param)):
+    legend_str.append(r"Random numbers histogram $(\mu = {}, \sigma = {})$".format(param[i][0], param[i][1]))
+
+plt.grid()
+plt.legend(legend_str)
+plt.show()
